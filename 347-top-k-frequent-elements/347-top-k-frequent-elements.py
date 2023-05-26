f852e7c841af39f897ae7457105266b09384bdc7
William Cross <46752543+Willem392@users.noreply.github.com>
2023-05-26 12:48:13 -0400

    Time: 126 ms (18.96%), Space: 21.8 MB (15.96%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,11 +3,22 @@ class Solution:
         freq = {}
         for n in nums:
             freq[n] = freq.get(n, 0) + 1
-        l, res = [], []
+        lists = [[]for i in range(len(nums) + 1)]
+        for key, val in freq.items():
+            lists[val].append(key)
+        res = []
+        for i in range(len(lists)-1, 0, -1):
+            for n in lists[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+            
+            
+        """l, res = [], []
         for elem in freq:
             l.append([freq[elem], elem])
         l = sorted(l)[::-1]
         for i in range(k):
             res.append(l[i][1])
-        return res
+        return res"""
             
