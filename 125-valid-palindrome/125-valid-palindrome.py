1bece0ef1509a96cbfd4ab055cf35f0547fcfcc1
William Cross <46752543+Willem392@users.noreply.github.com>
2023-05-26 14:06:43 -0400

    Time: 58 ms (53.33%), Space: 17.9 MB (15.18%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         l = []
         for ch in s:
             if ch.isalpha() or ch.isnumeric():
-                l.append(ch.lower())
+                l.append(ch)
         return l == l[::-1]
