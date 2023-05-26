0eec25f14f27f807316a387be16bc7a930b12da2
William Cross <46752543+Willem392@users.noreply.github.com>
2023-05-26 11:59:43 -0400

    Time: 131 ms (20.03%), Space: 22.3 MB (11.82%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,9 @@
-def count(s):
-    letters = [0] * 26
-    for ch in s:
-        letters[ord(ch)-97] += 1
-    return letters
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
         for s in strs:
-            res[tuple(count(s))].append(s)
+            letters = [0] * 26
+            for ch in s:
+                letters[ord(ch)-97] += 1
+            res[tuple(letters)].append(s)
         return res.values()
