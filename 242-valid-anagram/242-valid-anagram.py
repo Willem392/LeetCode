73692befc954fb128d7c3f27b5e6e0ae5353849c
William Cross <46752543+Willem392@users.noreply.github.com>
2023-05-26 11:26:40 -0400

    Time: 97 ms (6.09%), Space: 16.9 MB (34.27%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charS, charT = list(s), list(t)
-        charS.sort()
-        charT.sort()
-        return False if charS != charT else True
+        if len(s) != len(t):
+            return False 
+        sSet, tSet = {}, {}
+        for ch in s:
+            n = sSet.get(ch) if sSet.get(ch) != None else 0
+            sSet.update({ch:(n+1)})
+        for ch in t:
+            n = tSet.get(ch) if tSet.get(ch) != None else 0
+            tSet.update({ch:(n+1)})
+        if sSet == tSet:
+            return True
+        else:
+            return False
+        
