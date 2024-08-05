d178585fda4ebf4e62ebe3bf27581d7ef7d5a6c9
William Cross <46752543+Willem392@users.noreply.github.com>
2024-08-04 23:40:00 -0400

    Time: 124 ms (90.00%), Space: 16.9 MB (58.04%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,12 +1,9 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         valid_count = 0
-        i = 1
-        flowerbed.insert(0, 0)
-        flowerbed.append(0)
-        while i  < len(flowerbed) - 1:
-            if flowerbed[i] == 0:
-                if flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+        i = 0
+        while i  < len(flowerbed):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) -1 or flowerbed[i + 1] == 0):
                     valid_count += 1
                     i += 1
             i += 1
