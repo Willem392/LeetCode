4909e58011b8e669856e1e5613b4fd56b67af8c6
William Cross <46752543+Willem392@users.noreply.github.com>
2024-09-15 13:53:54 -0400

    Time: 61 ms (44.08%), Space: 16.5 MB (90.45%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        m = {}
+        for c in magazine:
+            m[c] = m.get(c, 0) + 1
+        for c in ransomNote:
+            if c not in m:
+                return False
+            if m[c] <= 0:
+                return False
+            m[c] = m.get(c) - 1
+        return True
